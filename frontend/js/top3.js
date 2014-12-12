0b9133ad32e4ce4cd26f4454fb96ae2251c009a4
FloDroidCoder <florian.genaudet@gmail.com>
2014-12-12 08:53:51 +0100

    Adding twitter fecther + update design + twitter walls

--- frontend/js/top3.js
+++ frontend/js/top3.js
@@ -1,19 +1,20 @@
 /** @jsx React.DOM */
 var Session = React.createClass({
   render: function() {
-  	var dateClass = 'session-date ' + (this.props.open ? 'open' : 'closed');
+  	var dateClass = 'session-date ' + (this.props.session.open ? 'open' : 'closed');
+
     return (
       <div className="session row">
-      	<div className="col-xs-4 like">
-      		{this.props.nbLike} <span className="glyphicon glyphicon-heart"/>
+      	<div className="col-xs-3 like">
+      		{this.props.session.nbLike} <span className="glyphicon glyphicon-heart"/>
       	</div>
-      	<div className="col-xs-8">
+      	<div className="col-xs-9">
         	<p>
-        		<a href={this.props.url}>{this.props.name}</a>
-        	 	by {this.props.author}
+        		<a href={this.props.session.url}>{this.props.session.name}</a>
+        	 	by <a href={this.props.session.author.url} className="authorLink">{this.props.session.author.name}</a>
         	 </p>
         	<p className={dateClass}>
-        		<span className="glyphicon glyphicon-time"></span>{this.props.date}
+        		<span className="glyphicon glyphicon-time"></span>{this.props.session.date}
         	</p>
         </div>
       </div>
@@ -25,12 +26,7 @@ var SessionList = React.createClass({
   render: function() {
   	var sessionNodes = this.props.data.map(function(session) {
   		return (
-  			<Session name={session.name} 
-  				date={session.date} 
-  				url={session.url} 
-  				open={session.open}
-  				nbLike={session.nbLike}
-  				author={session.author}></Session>
+  			<Session session={session}></Session>
   		);
   	});
     return (
