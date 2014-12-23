d10dcfda7f398608c1e51f255bacc9415d818ec4
Julien Roux <Julien.Roux@EWGECO021.ewgeco.local>
2014-12-23 14:50:38 +0000

    Modifying configuration to fix dependencies management problem

---
+++ backend/src/main/java/com/julien_roux/jug/quickies/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.julien_roux.jug.quickies;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+}
---
+++ backend/src/main/java/com/julien_roux/jug/quickies/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.julien_roux.jug.quickies;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+}
